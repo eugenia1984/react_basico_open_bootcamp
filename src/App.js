b6178cc3cda2abc85b0367c7a092c3493cc1b95a
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-11-30 17:28:20 -0300

    seguimos con los hooks

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TaskListComponent from "./components/container/task_list.jsx";
 import Ejemplo1 from './hooks/Ejemplo1';
 import Ejemplo2 from './hooks/Ejemplo2';
 import Ejemplo3 from './hooks/Ejemplo3';
+import Ejemplo4 from './hooks/Ejemplo4';
 
 function App() {
   return (
@@ -19,7 +20,13 @@ function App() {
       {/*<Ejemplo1 />*/}
       {/* Ejemplo de uso de hooks - useState y useEffect */}
       {/* <Ejemplo2 /> */}
+      {/* useState() y useContext() */}
       <Ejemplo3 />
+      {/* Ejemplo para entender props.children */}
+      <Ejemplo4 nombre="Euge">
+        {/* Todo lo que hay adentro es tratado como props.children*/}
+        <h3>Contenido del props.children</h3>
+      </Ejemplo4>
     </div>
   );
 }
