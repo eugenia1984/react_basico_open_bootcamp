9d4466601a668114b97bbe65687198fe03ae1e8a
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-11-30 16:44:39 -0300

    agrego el segundo ejemplo de hooks

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import './App.css';
 import GreetingF from './components/pure/greetingF';
 import TaskListComponent from "./components/container/task_list.jsx";
 import Ejemplo1 from './hooks/Ejemplo1';
+import Ejemplo2 from './hooks/Ejemplo2';
 
 function App() {
   return (
     <div className="App">
       {/* Componente propio Greeting - de clase */}
-      <Greeting name={"Eugenia"}></Greeting> 
+      {/*<Greeting name={"Eugenia"}></Greeting>*/} 
       {/* Componente propio GreetingF - funcional */}
-      <GreetingF name="Euge" />
+      {/*<GreetingF name="Euge" />*/}
       {/* Componente de listado de tareas */}
       <TaskListComponent />
       {/* Ejemplo de uso de hooks - useState */}
       <Ejemplo1 />
+      {/* Ejemplo de uso de hooks - useState y useEffect */}
+      <Ejemplo2 />
     </div>
   );
 }
